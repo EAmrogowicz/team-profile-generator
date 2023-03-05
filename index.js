4109d4aef130702da05c666fe672b1acf49da7bf
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-03-05 15:44:45 +0000

    working on inquirer prompts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+
+import inquirer from "inquirer";
+
 const path = require("path");
 const fs = require("fs");
 
@@ -10,6 +12,108 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+class teamProfile {
+  // Request for information
+  // EMPLOYEE - MANAGER
+  // Office number MISSING
+  askForManager = () => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the manager name:",
+          name: "managerName",
+        },
+        {
+          type: "input",
+          message: "What is the manager id number:",
+          name: "managerId",
+        },
+        {
+          type: "input",
+          message: "What is the manager email:",
+          name: "managerEmail",
+        },
+      ])
+      .then((answer) => {
+        console.log(answer);
+      });
+  };
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+  // EMPLOYEE - ENGINEER
+  askForEngineer = () => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the engineer name:",
+          name: "engineerName",
+        },
+        {
+          type: "input",
+          message: "What is the engineer id number:",
+          name: "engineerId",
+        },
+        {
+          type: "input",
+          message: "What is the engineer email:",
+          name: "engineerEmail",
+        },
+      ])
+      .then((answer) => {
+        console.log(answer);
+      });
+  };
 
+  // EMPLOYEE - INTERN
+  askForEngineer = () => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the intern name:",
+          name: "internName",
+        },
+        {
+          type: "input",
+          message: "What is the intern id number:",
+          name: "internId",
+        },
+        {
+          type: "input",
+          message: "What is the intern email:",
+          name: "internEmail",
+        },
+      ])
+      .then((answer) => {
+        console.log(answer);
+      });
+  };
+  // as for new employee
+  // askForNewEmployee = () => {
+  //   inquirer
+  //     .prompt([
+  //       {
+  //         type: "confirm",
+  //         message: "Do you want to add a new Employee?",
+  //         name: "choice",
+  //       },
+  //     ])
+  //     .then((answer) => {
+  //       // If the user says yes to another employee, run app again, otherwise quit the app and print result
+  //       if (answer.choice) {
+  //         const newEmployee = new Employee();
+  //       } else {
+  //         return;
+  //       }
+  //     });
+  // };
+  // quit() {
+  //   console.log("\nGoodbye!");
+  //   process.exit(0);
+  // }
+}
+//   writeFileAsync("./output/team.html", generateMarkdown(answers))
+// )
+// .then(() => console.log("Success!"))
+// .catch((err) => console.error(err));
